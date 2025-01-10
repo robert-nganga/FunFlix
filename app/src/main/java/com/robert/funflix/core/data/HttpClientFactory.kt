9a0e1a055d1c9c9a230053c93b6d383d5995667a
robert <robertnganga220@gmail.com>
2025-01-10 13:04:45 +0300

    Creating ktor client

---
+++ app/src/main/java/com/robert/funflix/core/data/HttpClientFactory.kt
@@ -0,0 +1,32 @@
+package com.robert.funflix.core.data
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.ANDROID
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object HttpClientFactory {
+
+    fun createClient(engine: HttpClientEngine): HttpClient{
+        return HttpClient(engine){
+            expectSuccess = true
+            install(Logging){
+                logger = Logger.ANDROID
+                level = LogLevel.ALL
+            }
+
+            install(ContentNegotiation){
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+    }
+}
