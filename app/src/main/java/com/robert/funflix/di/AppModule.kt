96de7660042452a68cf2bbf1c8481125563f4d1c
robert <robertnganga220@gmail.com>
2025-01-10 14:36:40 +0300

    Inject Http client in MainActivity

--- app/src/main/java/com/robert/funflix/di/AppModule.kt
+++ app/src/main/java/com/robert/funflix/di/AppModule.kt
@@ -1,7 +1,9 @@
 package com.robert.funflix.di
 
+import com.robert.funflix.core.data.HttpClientFactory
+import io.ktor.client.engine.okhttp.OkHttp
 import org.koin.dsl.module
 
 val appModule = module {
-
+    single { HttpClientFactory.createClient(engine = OkHttp.create()) }
 }
